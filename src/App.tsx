fd400a036379f08a8e2418b617336bc31cbd8f43
reyrey1616 <dabboyrey@gmail.com>
2022-01-24 13:37:22 +0800

    redeploy

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ const SecretaryRoutes = lazy(() => import("./routes/secretary.routes"));
 const LoginPage = lazy(() => import("./pages/secretary/login/login.page"));
 
 const PatientRegistrationPage = lazy(
-	() => import("./pages/patients/registration-form/registration.page")
+  () => import("./pages/patients/registration-form/registration.page")
 );
 
 const PatientsRoutes = lazy(() => import("./routes/patients.routes"));
@@ -19,35 +19,32 @@ const DoctorRoutes = lazy(() => import("./routes/doctor.routes"));
 const DoctorLogin = lazy(() => import("./pages/doctor/login/login.page"));
 
 const App: React.FC = () => {
-	useEffect(() => {
-		setAuthToken(localStorage.getItem("atkn"));
-	}, []);
-	return (
-		<>
-			<Suspense fallback={<Spinner />}>
-				<ErrorBoundary>
-					<Switch>
-						{/* <Route path="/admin" component={SecretaryRoutes} />
-						<Route path="/login" component={LoginPage} />
+  useEffect(() => {
+    setAuthToken(localStorage.getItem("atkn"));
+  }, []);
+  return (
+    <>
+      <Suspense fallback={<Spinner />}>
+        <ErrorBoundary>
+          <Switch>
+            <Route path="/admin" component={SecretaryRoutes} />
+            <Route path="/login" component={LoginPage} />
 
-						<Route path="/patients" component={PatientsRoutes} />
-						<Route
-							path="/patient-login"
-							component={PatientLoginPage}
-						/>
+            <Route path="/patients" component={PatientsRoutes} />
+            <Route path="/patient-login" component={PatientLoginPage} />
 
-						<Route path="/doctor" component={DoctorRoutes} />
-						<Route path="/doctor-login" component={DoctorLogin} />
+            <Route path="/doctor" component={DoctorRoutes} />
+            <Route path="/doctor-login" component={DoctorLogin} />
 
-						<Route
-							path="/patient-registration"
-							component={PatientRegistrationPage}
-						/> */}
-					</Switch>
-				</ErrorBoundary>
-			</Suspense>
-		</>
-	);
+            <Route
+              path="/patient-registration"
+              component={PatientRegistrationPage}
+            />
+          </Switch>
+        </ErrorBoundary>
+      </Suspense>
+    </>
+  );
 };
 
 export default App;
